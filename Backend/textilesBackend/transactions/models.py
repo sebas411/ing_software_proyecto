1a42cff9f58d92fb71aa22669bb25511c69ad93f
coutino568 <40869460+coutino568@users.noreply.github.com>
2021-04-26 13:49:01 -0600

    models_pt1

--- Backend/textilesBackend/transactions/models.py
+++ Backend/textilesBackend/transactions/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+class Employee(models.Model):
+	id= models.AutoField ( primary_key =True)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	hire_date = models.DateField()
+
+	
+class Transaction(models.Model):
+	id= models.AutoField ( primary_key =True)
+	creation_date = models.DateField(default=datetime.date.today)
+	confirmed = models.BooleanField(default=False)
+	amount = models.FloatField(default=0.0)
+	title = models.CharField(max_length=50)
+	details = models.CharField(max_length=300)
+	employee_ID = models.ForeignKey(Employee,on_delete=models.CASCADE)
+
