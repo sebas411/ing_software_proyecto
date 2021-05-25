c916dd5907d545936ec86cd582dffcf381e01e70
Martha30 <53914325+Martha30@users.noreply.github.com>
2021-05-24 20:04:08 -0600

    Update index.js
    css login

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,15 +1,40 @@
+import { Container } from '@material-ui/core';
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
 import "./style.css"
+import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router-dom'
+
 
 //Aquí iria el login
 
 const HomePage = () => {
 
-    return (
-    <div>
-        HOLI
+    const { push } = useHistory()
 
-    </div>)
+    return (
+
+        <Container>
+            
+            <div className="login-wrapper">
+                <h1>BIENVENIDOS</h1>
+            
+            <label>
+                <p>Usuario</p>
+                <input type="text" />
+            </label>
+            <label>
+                <p>Contraseña</p>
+                <input type="password" />
+            </label>
+            
+            <Button variant='Ingresar' onClick={() => push('registros')}>
+            Ingresar
+            </Button>
+            
+            </div>   
+       </Container>
+    );
 }
 
 export default HomePage
